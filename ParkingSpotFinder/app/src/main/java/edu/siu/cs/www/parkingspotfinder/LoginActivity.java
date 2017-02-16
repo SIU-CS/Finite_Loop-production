d8ffbf97bbedc8712b5f863ebb31dba9aae9683a
Jake Reed <jacob.reed95@siu.edu>
2017-02-15 23:54:30 -0600

    working on firebase auth next

--- ParkingSpotFinder/app/src/main/java/edu/siu/cs/www/parkingspotfinder/LoginActivity.java
+++ ParkingSpotFinder/app/src/main/java/edu/siu/cs/www/parkingspotfinder/LoginActivity.java
@@ -1,11 +1,16 @@
 package edu.siu.cs.www.parkingspotfinder;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -15,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
     private Button mLoginBtn;
+    private Button mRegisterBtn;
     private EditText emailTextField;
     private EditText nameTextField;
 
@@ -30,6 +36,7 @@ public class LoginActivity extends AppCompatActivity {
         mLoginBtn = (Button) findViewById(R.id.loginBtn);
         emailTextField = (EditText) findViewById(R.id.emailField);
         nameTextField = (EditText) findViewById(R.id.nameField);
+        mRegisterBtn = (Button) findViewById(R.id.registerBtn);
 
         // Listen for login button click
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
@@ -45,7 +52,26 @@ public class LoginActivity extends AppCompatActivity {
                 userDataMap.put("Email", email);
 
                 // Write changes to the database
-                mDatabase.push().setValue(userDataMap);
+                mDatabase.push().setValue(userDataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Data Stored", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Error: Unable to store data!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        // Starts the register activity
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new intent and start it
+                Intent registerActivity = new Intent(LoginActivity.this, RegisterActivitty.class);
+                startActivity(registerActivity);
             }
         });
     }
