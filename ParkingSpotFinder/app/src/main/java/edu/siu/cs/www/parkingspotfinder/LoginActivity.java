81a5b2accbcb9c67070f8f3059b63f5864e6f8a4
Jake Reed <jacob.reed95@siu.edu>
2017-02-15 19:45:57 -0600

    connecting and writing to firebase works

--- ParkingSpotFinder/app/src/main/java/edu/siu/cs/www/parkingspotfinder/LoginActivity.java
+++ ParkingSpotFinder/app/src/main/java/edu/siu/cs/www/parkingspotfinder/LoginActivity.java
@@ -2,12 +2,34 @@ package edu.siu.cs.www.parkingspotfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    private Button mLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Create an instance of the database connection to firebase
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        // Create the needed elements for the activity
+        mLoginBtn = (Button) findViewById(R.id.loginBtn);
+
+        // Listen for login button click
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                mDatabase.child("name").setValue("Jake");
+            }
+        });
     }
 }
