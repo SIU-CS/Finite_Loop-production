43d6acd6e18150a8a50468f4529385fd4d84be98
jake32321 <jacob.reed95@siu.edu>
2017-02-22 22:18:31 -0600

    added accout starter for those wanting to work on it

--- ParkingSpotFinder/app/src/main/java/edu/siu/cs/www/parkingspotfinder/AccountActivity.java
+++ ParkingSpotFinder/app/src/main/java/edu/siu/cs/www/parkingspotfinder/AccountActivity.java
@@ -1,28 +1,95 @@
 package edu.siu.cs.www.parkingspotfinder;
 
-import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AccountActivity extends AppCompatActivity {
 
-    private Button backArrowButton;
+    private FirebaseDatabase mDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference ref;
+
+    private TextView nameText;
+
+    private String name;
+    private String email;
+    private String userID;
+
+    private final static String TAG = "ACCOUNT_ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
-        backArrowButton = (Button) findViewById(R.id.backArrowButton);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        ref = mDatabase.getReference();
+        FirebaseUser user = mAuth.getCurrentUser();
+        userID = user.getUid();
 
-        backArrowButton.setOnClickListener(new View.OnClickListener() {
+        nameText = (TextView) findViewById(R.id.textView2);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onClick(View v) {
-                Intent menuActivityStart = new Intent(AccountActivity.this, MenuActivity.class);
-                startActivity(menuActivityStart);
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Log.d(TAG, "Logged In");
+                } else {
+                    Log.d(TAG, "Failed to login");
+                }
+            }
+        };
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                displayData(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
+
+    }
+
+    private void displayData(DataSnapshot dataSnapshot) {
+        for(DataSnapshot s : dataSnapshot.getChildren()){
+            User user = new User();
+            user.setName(s.child(userID).getValue(User.class).getName());
+            user.setEmail(s.child(userID).getValue(User.class).getName());
+
+            nameText.setText(user.getName());
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
