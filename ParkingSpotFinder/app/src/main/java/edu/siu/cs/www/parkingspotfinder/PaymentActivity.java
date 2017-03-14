311655ed4f145c51ef2fb7b52f8168ea91aa3239
jake32321 <jacob.reed95@siu.edu>
2017-03-14 16:03:33 -0500

    added new info view to the manage spot portion

--- ParkingSpotFinder/app/src/main/java/edu/siu/cs/www/parkingspotfinder/PaymentActivity.java
+++ ParkingSpotFinder/app/src/main/java/edu/siu/cs/www/parkingspotfinder/PaymentActivity.java
@@ -1,14 +1,17 @@
 package edu.siu.cs.www.parkingspotfinder;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class PaymentActivity extends AppCompatActivity {
 
-    private Button backArrowButton;
+    private Button backArrowButton, pageInfoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,21 @@ public class PaymentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_payment);
 
         backArrowButton = (Button) findViewById(R.id.backArrowButton);
+        pageInfoButton = (Button) findViewById(R.id.pageInfoButton);
+
+        pageInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(PaymentActivity.this)
+                        .setTitle("Information")
+                        .setMessage("Here you can see and add more time to your current time.  You can also leave the spot if you so choose.")
+                        .setIcon(android.R.drawable.ic_dialog_info)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // Does nothing except close
+                            }}).show();
+            }
+        });
 
         // Start the menu again if the user decides to go back
         backArrowButton.setOnClickListener(new View.OnClickListener() {
