e431633a661024421daf616cb0ca43a3bb03631f
jake32321 <jacob.reed95@siu.edu>
2017-03-26 15:53:18 -0500

    added create card tokens methods

--- ParkingSpotFinder/app/src/main/java/edu/siu/cs/www/parkingspotfinder/ManageSpot.java
+++ ParkingSpotFinder/app/src/main/java/edu/siu/cs/www/parkingspotfinder/ManageSpot.java
@@ -2,15 +2,24 @@ package edu.siu.cs.www.parkingspotfinder;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.simplify.android.sdk.CardEditor;
+import com.simplify.android.sdk.CardToken;
+import com.simplify.android.sdk.Simplify;
+
 public class ManageSpot extends AppCompatActivity {
 
-    private Button backArrowButton, pageInfoButton;
+    private final String SIMPLIFY_API_KEY = getResources().getString(R.string.simplify_api_key);
+
+    private Button backArrowButton, pageInfoButton, addMoreTimeButton;
+
+    private Simplify simplify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,39 @@ public class ManageSpot extends AppCompatActivity {
 
         backArrowButton = (Button) findViewById(R.id.backArrowButton);
         pageInfoButton = (Button) findViewById(R.id.pageInfoButton);
+        addMoreTimeButton = (Button) findViewById(R.id.addMoreTimeButton);
+
+        final CardEditor cardEditor = (CardEditor) findViewById(R.id.cardEditor);
+
+        simplify = new Simplify();
+        simplify.setApiKey(SIMPLIFY_API_KEY);
+
+        // Listen for the changes in state of the card editor
+        cardEditor.addOnStateChangedListener(new CardEditor.OnStateChangedListener() {
+            @Override
+            public void onStateChange(CardEditor cardEditor) {
+                addMoreTimeButton.setEnabled(cardEditor.isValid());
+            }
+        });
+
+        // Listen for when the user submits a new payment for processing
+        addMoreTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                simplify.createCardToken(cardEditor.getCard(), new CardToken.Callback(){
+                    @Override
+                    public void onSuccess(CardToken cardToken){
+                        // Add POST to PPaaS here
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable){
+                        // Handle errors here
+                    }
+                });
+            }
+        });
+
 
         pageInfoButton.setOnClickListener(new View.OnClickListener() {
             @Override
