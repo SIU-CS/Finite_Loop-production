d85a80db3474f5cce78efd34cb219316a8f64bf3
Jake Reed <jacob.reed95@siu.edu>
2017-04-18 23:20:04 -0500

    fixed listview issue

--- ParkingSpotFinder/app/src/main/java/edu/siu/cs/www/parkingspotfinder/SelectSpotActivity.java
+++ ParkingSpotFinder/app/src/main/java/edu/siu/cs/www/parkingspotfinder/SelectSpotActivity.java
@@ -1,7 +1,9 @@
 package edu.siu.cs.www.parkingspotfinder;
 
 import android.graphics.Color;
+import android.support.annotation.BoolRes;
 import android.support.annotation.IntegerRes;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,13 +17,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SelectSpotActivity extends AppCompatActivity {
 
     private final String TAG = "SELECT_SPOT_ACTRIVITY::";
+    private final Boolean DEBUG = true;
 
     private ListView spots;
+
+    private ArrayList<String> spotNames = new ArrayList<String>();
+    private ArrayAdapter<String> listViewAdapter;
 
     private DatabaseReference mRef;
 
@@ -30,14 +38,10 @@ public class SelectSpotActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_spot);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        ArrayList<String> spotNames = new ArrayList<String>();
-        final ArrayAdapter<String> listViewAdapter =
-                new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spotNames);
-        Bundle extras = getIntent().getExtras();
-
         spots = (ListView) findViewById(R.id.spotList);
-        spots.setAdapter(listViewAdapter);
+
+        Bundle extras = getIntent().getExtras();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
 
         if (extras != null){
             String tag = extras.getString("lot-tag");
@@ -47,15 +51,19 @@ public class SelectSpotActivity extends AppCompatActivity {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     for (DataSnapshot spot : dataSnapshot.getChildren()){
-                        int index = Integer.valueOf(spot.getKey().toString()) - 1;
-                        listViewAdapter.add(spot.child("name").getValue().toString());
-                        Log.d(TAG, "INDEX::"+spots.getCount());
-                        if (spot.child("state").getValue().toString().equals("READY")){
-                            spots.getAdapter().getView(index, null, spots).setEnabled(true);
-                            Log.d(TAG, String.valueOf(spots.getChildCount()));
-                        } else {
-                            spots.getAdapter().getView(index, null, spots).setClickable(false);
+//                        int index = Integer.valueOf(spot.getKey().toString()) - 1;
+                        spotNames.add(spot.child("name").getValue().toString());
+
+                        if(DEBUG){
+                            Log.d(TAG, Arrays.toString(spotNames.toArray()));
                         }
+                    }
+                    listViewAdapter = new ArrayAdapter<String>
+                            (SelectSpotActivity.this, android.R.layout.simple_spinner_dropdown_item, spotNames);
+                    spots.setAdapter(listViewAdapter);
+
+                    if(DEBUG){
+                        Log.d(TAG, String.valueOf(listViewAdapter.getCount()));
                     }
                 }
 
