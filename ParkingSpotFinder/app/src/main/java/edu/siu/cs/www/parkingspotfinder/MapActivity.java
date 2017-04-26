7ecf7338907187e8dcb6342386348021d5710f4d
Maha1990 <mahaalmaghshi@siu.edu>
2017-04-25 22:44:23 -0500

    AccountActivity (Code CleanUp)

--- ParkingSpotFinder/app/src/main/java/edu/siu/cs/www/parkingspotfinder/MapActivity.java
+++ ParkingSpotFinder/app/src/main/java/edu/siu/cs/www/parkingspotfinder/MapActivity.java
@@ -55,7 +55,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         final SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
