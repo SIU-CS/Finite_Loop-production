e4b1068c0ce25c49ed006c61c2f68889d0be413a
Maha1990 <mahaalmaghshi@siu.edu>
2017-05-03 11:41:44 -0500

    TestUnit (assignment5_CI)

---
+++ ParkingSpotFinder/app/src/main/java/edu/siu/cs/www/parkingspotfinder/ExampleUniteTest.java
@@ -0,0 +1,9 @@
+package edu.siu.cs.www.parkingspotfinder;
+
+import static junit.framework.Assert.assertEquals;
+
+public class ExampleUniteTest {
+    public void addition_isCorrect() throws Exception {
+        assertEquals(6, 3+3);
+    }
+}
